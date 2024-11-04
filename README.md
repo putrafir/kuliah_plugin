10ba5b1d7234bea94456647305ce16edb4fd987a
putrafir <141028423+putrafir@users.noreply.github.com>
2024-11-04 15:57:11 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![hasil praktikum](<Tangkapan Layar 2024-11-04 pukul 15.54.50.png>)
---
+++ lib/widget/takepicture_screen copy.dart
@@ -0,0 +1,62 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class TakepictureScreen extends StatefulWidget {
+  const TakepictureScreen({super.key, required this.camera});
+
+  final CameraDescription camera;
+
+  @override
+  State<TakepictureScreen> createState() => _TakepictureScreenState();
+}
+
+class _TakepictureScreenState extends State<TakepictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(widget.camera, ResolutionPreset.high);
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Take a picture - NIM Anda: 362358302028'),
+      ),
+      body: FutureBuilder(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            Navigator.pop(context, image.path);
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera),
+      ),
+    );
+  }
+}
